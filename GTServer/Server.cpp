019f5c7a794d8a4e8473a03a534ce6f8fc96737e
Yanny Esteban <yannyesteban@hotmail.com>
2021-09-17 12:25:21 -0400

    ok

--- GTServer/Server.cpp
+++ GTServer/Server.cpp
@@ -946,7 +946,7 @@ namespace GT {
 			printf("%8d", it->second.version_id);
 			printf("%6d\n", it->second.type);
 
-			if (it->second.type == 2 && timeInSeconds > keepAliveTime) {
+			if (it->second.type == 0 && timeInSeconds > keepAliveTime) {
 				printf("%50s\n", "-- DISCONECTING TO:");
 				printf("%10d", it->second.header);
 				printf("%18s", it->second.address);
