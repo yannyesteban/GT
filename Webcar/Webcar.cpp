7dc4983978601e36242dceed019b4e49a3ca221f
Yanny Esteban <yannyesteban@hotmail.com>
2021-11-01 14:53:00 -0400

    ok

--- Webcar/Webcar.cpp
+++ Webcar/Webcar.cpp
@@ -819,7 +819,7 @@ namespace WC {
 			//cout << "WEBCAR ERR: SQLException\n" ;
 			
 			cout << endl << endl << "WEBCAR ERR: SQLException in " << __FILE__;
-			//cout << track << endl;
+			cout << track << endl;
 			cout << endl << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
 			cout << endl << "WEBCAR ERR: " << e.what();
 			cout << endl << " (MySQL error code: " << e.getErrorCode();
